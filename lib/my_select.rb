aa3c8ff639cd351177336ab803747f0be7f47da2
Maria Ramirez <nanyramirez0@gmail.com>
2016-12-19 23:39:56 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,15 @@
 def my_select(collection)
  # your code here!
+selection=[]
+ i=0
+ while i<collection.length
+   passes=yield(collection[i])
+   selection.push(collection[i]) if passes==true
+   i+=1
+ end
+ selection
+end
+
+my_select([1, 2, 3, 4]) do |element|
+  element%2==0
 end
